449c80890b1dd8397726c88df1adad187297d7a7
George Boakye <georgekwamekumiboakye@gmail.com>
2023-03-15 18:18:51 +0000

    linked list

---
+++ linked-list/index.js
@@ -0,0 +1,12 @@
+const LinkedList = require('./LinkedList')
+
+
+const example =  new LinkedList();
+
+example.addToHead('George');
+example.addToTail('Kwame');
+example.addToTail('Kumi');
+example.addToTail('Boakye');
+
+
+example.printList()
